4be5e8115241cc6e721b9a56495ae79042a23702
Liza Zhukova <Elishkaaaa@yandex.ru>
2023-05-08 23:57:25 +0300

    fix: добавлен комментарий

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,6 +3,11 @@ const UnauthorizedError = require('../utils/errors/unauthorizedError');
 
 const { NODE_ENV, JWT_SECRET = 'dev-secret' } = process.env;
 
+// ошибки 500 больше нет, но при открытии профиля выдает 401,
+// якобы не авторизован профиль, поэтому ничего сделать нельзя
+// просидела весь день, не смогла понять в чем проблема,
+// если поможете или подскажите в чем может быть ошибка, буду очень благодарна
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
