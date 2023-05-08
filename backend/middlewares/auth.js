01e935a0af77e21f336de277c837a86154457d2d
Liza Zhukova <Elishkaaaa@yandex.ru>
2023-05-08 19:10:41 +0300

    fix: добавлена переменная окружения

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../utils/errors/unauthorizedError');
 
-const { JWT_SECRET = 'dev-secret' } = process.env;
+const { NODE_ENV, JWT_SECRET = 'dev-secret' } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
